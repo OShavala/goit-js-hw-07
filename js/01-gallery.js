c576883aecca8355f17a0e1633f160a7a36c2679
Olena Shavala <olena.shvl@gmail.com>
2023-05-21 09:58:56 +0300

    first fixation

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+function createGalleryItems(galleryItems) {
+  let galleryElements = galleryItems.map((item) => {
+    let galleryItem = document.createElement("li");
+    galleryItem.classList.add("gallery__item");
+
+    let galleryLink = document.createElement("a");
+    galleryLink.classList.add("gallery__link");
+    galleryLink.href = item.original;
+
+    let galleryImg = document.createElement("img");
+    galleryImg.classList.add("gallery__image");
+    galleryImg.src = item.preview;
+    galleryImg.dataset.source = item.original;
+    galleryImg.alt = item.description;
+    galleryLink.appendChild(galleryImg);
+
+    galleryItem.appendChild(galleryLink);
+
+    return galleryItem;
+  });
+  galleryEl.append(...galleryElements);
+}
+createGalleryItems(galleryItems);
+
+let instance;
+
+function onImgClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  instance = basicLightbox.create(`
+      <img src='${event.target.dataset.source}' width="800" height="auto">
+   `);
+  instance.show();
+}
+
+galleryEl.addEventListener("click", onImgClick);
